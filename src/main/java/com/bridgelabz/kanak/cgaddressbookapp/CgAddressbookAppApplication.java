94c90c1f86f2343a064ba552d0845b877b811404
Kanak Rajput <rajputkanak078@gmail.com>
2025-02-26 23:41:40 +0530

    [kanak]initial commit

---
+++ src/main/java/com/bridgelabz/kanak/cgaddressbookapp/CgAddressbookAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.kanak.cgaddressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgAddressbookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgAddressbookAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/kanak/cgaddressbookapp/CgAddressbookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.kanak.cgaddressbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgAddressbookAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
